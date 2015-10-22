bd39c0d0c575aa5cd501f3949ba75b3489f2b143
Greycrazygamer <dnola@calpoly.edu>
2015-10-22 14:03:04 -0700

    working on it

--- Board.java
+++ Board.java
@@ -1,3 +1,6 @@
+import java.awt.Point;
+import java.util.LinkedList;
+
  
 public class Board
 {
@@ -7,8 +10,41 @@ public class Board
 	//board doesnt not move pieces, simply updates locations based on piece methods
 	
 	//8x8 grid
-	  //0 1 2 3 4 5 6 7
-	//a
-	//b
-	//
+	  //0 1 2 3 4 5 6 7  x
+	//0
+	//1
+	//y
+	
+	private int grid[][];
+	private static Board instance = null;
+	protected Board()
+	{
+		this.grid = new int[8][8];
+	}
+	
+	public static Board getInstance()
+	{
+		if(instance==null)
+			instance = new Board();
+		return instance;
+	}
+	public LinkedList<Point>initalPieces(COLOR color)
+	{
+		if(color.equals(color.RED))
+		{
+			LinkedList<Point> initalList = new LinkedList<Point>();
+			for(int y =0; y < 3; y++)
+			{
+				for(int x =0; x<8; x++)
+				{
+					if(y%2==0 && x%2==0)
+						initalList.add(new Point(x,y));
+				}
+			}
+		}
+		else
+		{
+			
+		}
+	}
 }
